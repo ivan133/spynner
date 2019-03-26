38c22908ebbdedc5e01971f42aa21bc692e64de6
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2019-03-26 14:57:41 +0100

    Back to development: 2.24

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 import os
 from distutils.cmd import Command
 
-version = '2.23'
+version = '2.24.dev0'
 url = "https://github.com/kiorky/spynner"
 
 def read(rnames):
